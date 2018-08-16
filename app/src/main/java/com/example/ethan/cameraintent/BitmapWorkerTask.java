c858a2dbc799e670c017ca81ad29d7ec506af7a4
ldtuanAPCS <ldtuan@apcs.vn>
2018-08-16 23:33:37 +0700

    AsyncDrawable

--- app/src/main/java/com/example/ethan/cameraintent/BitmapWorkerTask.java
+++ app/src/main/java/com/example/ethan/cameraintent/BitmapWorkerTask.java
@@ -11,24 +11,68 @@ import java.lang.ref.WeakReference;
 public class BitmapWorkerTask extends AsyncTask<File, Void, Bitmap>{
 
     WeakReference<ImageView> imageViewReferences;
+    final static int TARGET_IMAGE_VIEW_WIDTH = 200;
+    final static int TARGET_IMAGE_VIEW_HEIGHT = 200;
+    private File mImageFile;
 
     public BitmapWorkerTask(ImageView imageView){
-        imageViewReferences = new WeakReference<>(imageView);
+        imageViewReferences = new WeakReference<ImageView>(imageView);
     }
 
     @Override
     protected Bitmap doInBackground(File... params) {
-        return BitmapFactory.decodeFile(params[0].getAbsolutePath());
+        mImageFile = params[0];
+        return decodeBitmapFromFile(params[0]);
     }
 
     @Override
     protected void onPostExecute(Bitmap bitmap) {
+        /*
         if (bitmap != null && imageViewReferences != null){
             ImageView viewImage = imageViewReferences.get();
             if (viewImage != null){
                 viewImage.setImageBitmap(bitmap);
             }
         }
-        super.onPostExecute(bitmap);
+        */
+        if (isCancelled()){
+            bitmap = null;
+        }
+        if (bitmap != null && imageViewReferences != null){
+            ImageView imageView = imageViewReferences.get();
+            BitmapWorkerTask bitmapWorkerTask = ImageAdapter.getBitmapWorkerTask(imageView);
+            if (this == bitmapWorkerTask && imageView != null){
+                imageView.setImageBitmap(bitmap);
+            }
+        }
+    }
+
+    private int calculateSampleSize(BitmapFactory.Options bmOptions){
+        final int photoWidth = bmOptions.outWidth;
+        final int photoHeight = bmOptions.outHeight;
+        int scaleFactor = 1;   // full size of an image
+
+        if (photoWidth > TARGET_IMAGE_VIEW_WIDTH || photoHeight > TARGET_IMAGE_VIEW_HEIGHT){
+            final int halfPhotoWidth = photoWidth / 2;
+            final int halfPhotoHeight = photoHeight / 2;
+            while (halfPhotoWidth/scaleFactor > TARGET_IMAGE_VIEW_WIDTH
+                    || halfPhotoHeight/scaleFactor > TARGET_IMAGE_VIEW_HEIGHT){
+                scaleFactor *= 2;
+            }
+        }
+        return scaleFactor;
+    }
+
+    private Bitmap decodeBitmapFromFile (File imageFile){
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(imageFile.getAbsolutePath(), bmOptions);
+        bmOptions.inSampleSize = calculateSampleSize(bmOptions);
+        bmOptions.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFile(imageFile.getAbsolutePath(), bmOptions);
+    }
+
+    public File getImageFile(){
+        return mImageFile;
     }
 }
--- app/src/main/java/com/example/ethan/cameraintent/ImageAdapter.java
+++ app/src/main/java/com/example/ethan/cameraintent/ImageAdapter.java
@@ -1,7 +1,10 @@
 package com.example.ethan.cameraintent;
 
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -9,9 +12,28 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import java.io.File;
+import java.lang.ref.WeakReference;
+import java.util.List;
 
 public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder>{
     private File imagesFile;
+    private Bitmap placeHolderBitmap;
+
+    public static class AsyncDrawable extends BitmapDrawable{
+        final WeakReference<BitmapWorkerTask> taskReference;
+
+        public AsyncDrawable(Resources resources,
+                             Bitmap bitmap,
+                             BitmapWorkerTask bitmapWorkerTask){
+            super(resources, bitmap);
+            taskReference = new WeakReference(bitmapWorkerTask);
+
+        }
+
+        public BitmapWorkerTask getBitmapWorkerTask(){
+            return taskReference.get();
+        }
+    }
 
     public ImageAdapter(File folderFile){
         imagesFile = folderFile;
@@ -28,10 +50,16 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder>{
     @Override
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
         File imageFile = imagesFile.listFiles()[position];
-//        Bitmap imageBitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
-//        viewHolder.getImageView().setImageBitmap(imageBitmap);
-        BitmapWorkerTask workerTask = new BitmapWorkerTask(viewHolder.getImageView());
-        workerTask.execute(imageFile);
+        //BitmapWorkerTask workerTask = new BitmapWorkerTask(viewHolder.getImageView());
+        //workerTask.execute(imageFile);
+        if (checkBitmapWorkerTask(imagesFile, viewHolder.getImageView())){
+            BitmapWorkerTask bitmapWorkerTask = new BitmapWorkerTask(viewHolder.getImageView());
+            AsyncDrawable asyncDrawable = new AsyncDrawable(viewHolder.getImageView().getResources(),
+                    placeHolderBitmap,
+                    bitmapWorkerTask);
+            viewHolder.getImageView().setImageDrawable(asyncDrawable);
+            bitmapWorkerTask.execute(imageFile);
+        }
     }
 
     @Override
@@ -51,4 +79,30 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder>{
             return imageView;
         }
     }
+
+    public static boolean checkBitmapWorkerTask(File imagesFile, ImageView imageView){
+        BitmapWorkerTask bitmapWorkerTask = getBitmapWorkerTask(imageView);
+        if (bitmapWorkerTask != null){
+            final File workerFile = bitmapWorkerTask.getImageFile();
+            if (workerFile != null){
+                if (workerFile != imagesFile){
+                    bitmapWorkerTask.cancel(true);
+                } else {
+                    //bitmap worker task is the same as the imageview is expecting
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static BitmapWorkerTask getBitmapWorkerTask(ImageView imageView){
+        Drawable drawable = imageView.getDrawable();
+        if (drawable instanceof AsyncDrawable){
+            AsyncDrawable asyncDrawable = (AsyncDrawable) drawable;
+            return asyncDrawable.getBitmapWorkerTask();
+        }
+        return null;
+    }
+
 }
